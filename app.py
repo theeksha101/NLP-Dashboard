88ec300e57570f9b27d0bc10b6a4bdcc7d08aa5e
theeksha101 <deekshaw25@gmail.com>
2022-08-27 16:46:09 +0530

    Initializing the repo

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, url_for, redirect, flash
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    results = []
+    score_dict = {}
+    if request.method == 'POST':    
+        text = request.form['data']
+        if text == "":
+            flash("Please enter some text first")
+        else:
+            results, score_dict = sentiment_scores(text)
+    return render_template('index.html', results=results, score_dict=score_dict)
+
+
+def sentiment_scores(data):
+    sid_obj = SentimentIntensityAnalyzer()
+    sentiment_dict = sid_obj.polarity_scores(data)
+    list1 = []
+    list1.append("Your text is, ")
+    list1.append("{} % Negative".format(sentiment_dict['neg']*100))
+    list1.append("{} % Neutral".format(sentiment_dict['neu']*100))
+    list1.append("{} % Positive".format(sentiment_dict['pos']*100))
+    return list1, sentiment_dict 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+           
